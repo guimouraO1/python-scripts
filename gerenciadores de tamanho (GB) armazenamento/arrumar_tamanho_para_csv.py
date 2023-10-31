6824a8cf2c31b9c3930fc02e473704eed551cef8
Guilherme <101018681+guimouraO1@users.noreply.github.com>
2023-10-31 09:58:05 -0300

    Update arrumar_tamanho_para_csv.py

--- gerenciadores de tamanho (GB) armazenamento/arrumar_tamanho_para_csv.py
+++ gerenciadores de tamanho (GB) armazenamento/arrumar_tamanho_para_csv.py
@@ -11,50 +11,37 @@
 #  Atualizacao: 07/07/2023                                                #
 ###########################################################################
 
-# Escreve no arquivo csv os dados recebidos da função ler_arquivos
 def escrever_arquivo(nome_arquivo, dia, total_objects, total_size):
     with open(f'{nome_arquivo}.csv', 'a') as arquivo:
         arquivo.write(f'{dia} ')
         arquivo.write(f'{total_objects} ')
         arquivo.write(f'{total_size} \n')
 
-# lê o arquivo txt para retornar os parametros pedidos
 def ler_arquivo(arquivo_entrada):
-    # Arquivo nome : para saida do CSV
     arquivo_saida = 'saida'
-
-    # abre o arquivo para leitura(r) e salva na var arquivo
     with open(f'{arquivo_entrada}.txt', 'r') as arquivo:
-        # as linhas são separadas por '\n' e atribuidas a variável linhas
-        linhas = arquivo.read().splitlines()
-    # Percorre todas as linhas
-    for linha in linhas:
-        # Se a linhas começar com : a variável dia vai receber a linha inteira
-        if linha.startswith('rclone size /mnt/storage/level1b/ano2023/'):
-            dia = linha.strip('-').strip()
-            # dia recebe o dia só que separado do resto resultado ex: 001 ou 120 restando apenas os números
-            dia = dia.replace('rclone size /mnt/storage/level1b/ano2023/dia','')
-        # Se a linha começar com Total Objects a linha é separada com ' ' com .split, a variável total_objects recebe a casa [2] que tem a quantidade de objetos
-        elif linha.startswith('Total objects:'):
-            total_objects = linha.split(' ')[2]
-            # Apenas tira o 'K', e o '.' da string
-            total_objects = total_objects.replace(f'k', '').replace('.', '')
-        # Se a linha começar com Total size: a linha é separada com ' ' com .split, a variável total_size recebe a casa [2] que contém os GB
-        elif linha.startswith('Total size:'):
-            total_size = linha.split(' ')[2]
-            # Apenas tira o 'GiB' da string
-            total_size = total_size.replace('GiB', '')
-        
-        try:
-            # Grava as informações no arquivo de saída
-            escrever_arquivo(arquivo_saida, dia, total_objects, total_size)
-            print('Arquivo criado')
-        # Printa o erro
-        except Exception as error:
-            print(f'Erro ao deletar os arquivos: {str(error)}')
+        linhas = arquivo.read().split('\n')
+    
+    dia = None
+    total_objects = None
+    total_size = None
 
-            
+    for linha in linhas:
+        if linha.startswith('rclone size /mnt/storage/level1b/ano2023/'):
+            if dia is not None:
+                # Escreve as informações associadas ao dia anterior
+                escrever_arquivo(arquivo_saida, dia, total_objects, total_size)
+            dia = linha.split('/')[-1].strip()
+            total_objects = None
+            total_size = None
+        elif linha.startswith('Total objects:'):
+            total_objects = linha.split(' ')[2].replace('k', '').replace('.', '')
+        elif linha.startswith('Total size:'):
+            total_size = linha.split(' ')[2].replace('GiB', '')
+    
+    # Escreve as informações para o último dia
+    if dia is not None and total_objects is not None and total_size is not None:
+        escrever_arquivo(arquivo_saida, dia, total_objects, total_size)
 
 arquivo_entrada = 'level1'
-
 ler_arquivo(arquivo_entrada)
